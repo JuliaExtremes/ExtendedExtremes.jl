46799cf600a9ab4ede224d86b40976457b34a854
Gabriel <gabriel.gobeil199@gmail.com>
2020-07-07 09:59:17 -0400

    add utils file

---
+++ src/utils.jl
@@ -0,0 +1,10 @@
+# macro for argument checking (taken from Distributions.jl)
+
+macro check_args(D, cond)
+    quote
+        if !($(esc(cond)))
+            throw(ArgumentError(string(
+                $(string(D)), ": the condition ", $(string(cond)), " is not satisfied.")))
+        end
+    end
+end
