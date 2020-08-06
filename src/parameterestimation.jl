06344bc7441fe974b468b7161e0470cff34e15f1
philemongamet <39191421+philemongamet@users.noreply.github.com>
2020-08-06 11:14:09 -0400

    Update parameterestimation.jl
    encapsulates the positive parameters of the EGPnormal model in an exponential to remove constraints during estimation

--- src/parameterestimation.jl
+++ src/parameterestimation.jl
@@ -127,30 +127,27 @@ function EGPbetapowerfit(data::Array{<:Real,1}; initialvalues::Vector{<:Real}=Fl
 end
 
 function EGPnormalfit(data::Array{<:Real,1}; initialvalues::Vector{<:Real}=Float64[])
-    inner_optimizer = NelderMead()
-    lower = [0, -Inf, 0]
-    upper = [Inf, Inf, Inf]
 
     if isempty(initialvalues)
-        σ₀, ξ₀, κ₀ = [1, 0.15, 1]  # à remplacer par une fonction (getinitialvalue)
-        initialvalues = [σ₀, ξ₀, κ₀]
+        logσ₀, ξ₀, logκ₀ = [0, 0.15, 0]  # à remplacer par une fonction (getinitialvalue)
+        initialvalues = [logσ₀, ξ₀, logκ₀]
     end
 
-    function loglike(σ::Real, ξ::Real, κ::Real)
-        pd = EGPnormal(σ, ξ, κ)
+    function loglike(logσ::Real, ξ::Real, logκ::Real)
+        pd = EGPnormal(exp(logσ), ξ, exp(logκ))
         ll = sum(logpdf.(pd, data))
         return ll
     end
 
     fobj(θ) = -loglike(θ...)
 
-    res = optimize(fobj, lower, upper, initialvalues, Fminbox(inner_optimizer))
+    res = optimize(fobj, initialvalues)
 
     if Optim.converged(res)
-        σ̂, ξ̂, κ̂ = [Optim.minimizer(res)...]
+        σ̂, ξ̂, κ̂ = [exp(Optim.minimizer(res)[1]), Optim.minimizer(res)[2], exp(Optim.minimizer(res)[3])]
     else
         @warn "The maximum likelihood algorithm did not find a solution. Maybe try with different initial values or with another method. The returned values are the initial values."
-        σ̂, ξ̂, κ̂  = [initialvalues...]
+        σ̂, ξ̂, κ̂  = [exp(initialvalues[1]), initialvalues[2], exp(initialvalues[3])]
     end
 
     fittedmodel = EGPnormal(σ̂, ξ̂, κ̂)
