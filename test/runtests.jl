964cefd81c39251fd622885acb1d6d48dc513ecc
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2023-03-30 22:17:52 -0400

    delete old tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
-using ExtendedExtremes
-using Extremes, Random, Test
+# using ExtendedExtremes
+# using Extremes, Random, Test
 
-# Set the seed for reproductible test results
-Random.seed!(12)
+# # Set the seed for reproductible test results
+# Random.seed!(12)
 
-@testset "ExtendedExtremes.jl" begin
-    include("reproducingNaveauResults.jl")
-    include("parameterestimation.jl")
-    include("EGPpower.jl")
-    include("EGPbeta.jl")
-    include("EGPbetapower.jl")
-end;
+# @testset "ExtendedExtremes.jl" begin
+#     include("reproducingNaveauResults.jl")
+#     include("parameterestimation.jl")
+#     include("EGPpower.jl")
+#     include("EGPbeta.jl")
+#     include("EGPbetapower.jl")
+# end;
