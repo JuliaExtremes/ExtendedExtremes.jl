e65662535dc825ff14ca875ce97bf891e58e2bc4
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2026-06-11 09:23:12 -0400

    verify fit_mle

--- performance.jl
+++ performance.jl
@@ -15,3 +15,5 @@ pd = ExtendedGeneralizedPareto(TBeta(.8), GeneralizedPareto(1,0))
 y = rand(Random.seed!(1), pd, 5000)
 
 @time logpdf(pd, y); @time logpdf(pd, y);
+
+@time fd = fit_mle(ExtendedGeneralizedPareto{TBeta}, y); @time fit_mle(ExtendedGeneralizedPareto{TBeta}, y);
